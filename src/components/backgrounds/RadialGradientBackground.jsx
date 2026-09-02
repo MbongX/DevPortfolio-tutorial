1c322eb2d1a85f115e40ba1233d341405affa449
MbongX <Mbongenimahlangu203@outlook.com>
2026-09-02 05:21:17 +0200

    feat: add About section and integrate into main application

--- src/components/backgrounds/RadialGradientBackground.jsx
+++ src/components/backgrounds/RadialGradientBackground.jsx
@@ -1,6 +1,124 @@
 import React from 'react'
 
-const RadialGradientBackground = () => {
+const RadialGradientBackground = ({varient="hero", gradients=[]}) => {
+
+    const variants = {
+        hero: [
+            {
+                position: 'top-1 left-1 -translate-x-1/2 -translate-y-1/2',
+                size: 'w-[1400px] h-[1400px]',
+                colors: [
+                    {
+                        color: 'rgba(141,255,105,0.25)',
+                        stop: '100%'
+                    },
+                    {
+                        color: 'rgba(141,255,105,0.45)',
+                        stop: '100%'
+                    },
+                    {
+                        color: 'rgba(141,255,105,0.5)',
+                        stop: '100%'
+                    },
+                    {
+                        color: 'rgba(141,255,105,0.45)',
+                        stop: '100%'
+                    },
+                    {
+                        color: 'rgba(144,255,105,0.25)',
+                        stop: '100%'
+                    }
+                ],
+                blur: '0px',
+                opacity:0.5
+            },
+            {
+                position: 'top-1 left-1',
+                size: 'w-[1400px] h-[1400px]',
+                colors: [
+                    {
+                        color: 'rgba(144,255,105,0.25)',
+                        stop: '100%'
+                    },
+                    {
+                        color: 'rgba(144,255,105,0.45)',
+                        stop: '100%'
+                    },
+                    {
+                        color: 'rgba(144,255,105,0.5)',
+                        stop: '100%'
+                    },
+                    {
+                        color: 'rgba(144,255,105,0.45)',
+                        stop: '100%'
+                    },
+                    {
+                        color: 'rgba(141,255,105,0.25)',
+                        stop: '100%'
+                    }
+                ],
+                blur: '0px',
+                opacity: 0.5
+            },
+            {
+                position: 'bottom-1 right-1',
+                size: 'w-[1400px] h-[1400px]',
+                colors: [
+                    {
+                        color: 'rgba(144,255,105,0.25)',
+                        stop: '100%'
+                    },
+                    {
+                        color: 'rgba(144,255,105,0.45)',
+                        stop: '100%'
+                    },
+                    {
+                        color: 'rgba(141,255,105,0.5)',
+                        stop: '100%'
+                    },
+                    {
+                        color: 'rgba(141,255,105,0.45)',
+                        stop: '100%'
+                    },
+                    {
+                        color: 'rgba(144,255,105,0.25)',
+                        stop: '100%'
+                    },
+                ],
+                blur: '0px',
+                opacity: 0.5
+            }
+        ],
+        about: [
+            {
+                position: 'bottom-1 right-1',
+                size: 'w-[1400px] h-[1400px]',
+                colors: [
+                    {
+                        color: '',
+                        stop: ""
+                    },
+                    {
+                        color: '',
+                        stop: ""
+                    },
+                    {
+                        color: '',
+                        stop: ""
+                    },
+                    {
+                        color: '',
+                        stop: ""
+                    },
+                    {
+                        color: '',
+                        stop: ""
+                    },
+                ],
+            }
+        ],
+    }
+
     return (
         <div>RadialGradientBackground</div>
     )
