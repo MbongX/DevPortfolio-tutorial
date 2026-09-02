0e2b89295855f172b6cbc892d1fd8c2dc0b5129d
MbongX <Mbongenimahlangu203@outlook.com>
2026-09-02 06:29:37 +0200

    feat: enhance About section with skills display and animations (skeleton)

--- src/components/backgrounds/RadialGradientBackground.jsx
+++ src/components/backgrounds/RadialGradientBackground.jsx
@@ -95,32 +95,53 @@ const RadialGradientBackground = ({varient="hero", gradients=[]}) => {
                 size: 'w-[1400px] h-[1400px]',
                 colors: [
                     {
-                        color: '',
-                        stop: ""
+                        color: '141,255,105,0.25',
+                        stop: "100%"
                     },
                     {
-                        color: '',
-                        stop: ""
+                        color: '141,255,105,0,45',
+                        stop: "100%"
                     },
                     {
-                        color: '',
-                        stop: ""
+                        color: '141,255,105,0.5',
+                        stop: "100%"
                     },
                     {
-                        color: '',
-                        stop: ""
+                        color: '141,255,105,0.45',
+                        stop: "100%"
                     },
                     {
-                        color: '',
-                        stop: ""
-                    },
+                        color: '141,255,105,0.25',
+                        stop: "100%"
+                    }
                 ],
+                blur: '0px',
+                opacity: 0.5
             }
         ],
-    }
+    };
+
+    const activeGradients = varient === 'custom' ? gradients : variants[varient] || varient.hero;
+
+    const generateGradient = (colors) => {
+        const colorStops = colors.map(({color, stop}) => `${color} ${stop}`).join(', ');
+        return `radial-gradient(circle at center, transparent 0%, transparent 30%, ${colorStops}, transparent 60%, transparent 100%)`;
+    };
 
     return (
-        <div>RadialGradientBackground</div>
+        <div className="absolute inset-0 overflow-hidden pointer-events-none">
+            {activeGradients.map((gradient, index) => (
+                <div
+                    key={index}
+                    className={`absolute ${gradient.position} ${gradient.size} rounded-full`}
+                    style={{
+                        background:  generateGradient(gradient.colors),
+                        filter: `blur(${gradient.blur})`,
+                        opacity: gradient.opacity
+                    }}
+                />
+            ))}
+        </div>
     )
 }
 
